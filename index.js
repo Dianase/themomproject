d2e1433a8af415b8104b6e16f7a0f31b26e02e40
Diana Tineo <diana.tineo1@gmail.com>
2022-05-08 15:19:32 -0400

    initial commit/ pseudocode

---
+++ index.js
@@ -0,0 +1,11 @@
+//write a program that spits out the lyrics to the song "99 bottles of beer on the wall"
+
+//start with 99 bottles and work down to 0 bottles. 
+function beerSong() {
+//keep track of the number of bottles by storing the initial value in a variable called i and working our way down 
+//by subtracting one until there are no more bottles left.
+
+  
+}
+beerSong();
+
