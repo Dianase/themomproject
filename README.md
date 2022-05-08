e43799f06ca2bb030451b4b74cec4fd5164b9e40
Diana Tineo <diana.tineo1@gmail.com>
2022-05-08 16:52:00 -0400

    added readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Javascript application - that prints lyrics to the song 99 bottles of beer on the wall.
+
+## To RUN 
+### Step 1-  Download + Install Node v16.9.1
+
+### Step 2- Open terminal where project is cloned
+
+### Step 3- To run this program execute the following command in terminal 
+``` node index.js```
+
+## Expected Output -
+### 99 bottles of beer on the wall, 99 bottles of beer. "Take one down and pass it around, 98 bottles of beer on the wall.
+### 98 bottles of beer on the wall, 98 bottles of beer. "Take one down and pass it around, 97 bottles of beer on the wall.
+### 97 bottles of beer on the wall, 97 bottles of beer. "Take one down and pass it around, 96 bottles of beer on the wall.
+### ...
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-//write a program that spits out the lyrics to the song "99 bottles of beer on the wall"
+//write a program that outputs the lyrics to the song "99 bottles of beer on the wall"
 
 //start with 99 bottles and work down to 0 bottles. 
 function beerSong() {
-//keep track of the number of bottles by storing the initial value in a variable called i and working our way down 
+//keep track of the number of bottles by storing the initial value (99 beers) in a variable called i and working our way down 
 //by subtracting one until there are no more bottles left.
 for (let i = 99; i >= 0; i--) {
   //if we have 0 bottles left. We print the following message:
   if(i === 0) {
     console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   }
-  //if we have more than 1 bottle left. We print the following: we use i as the current number of bottles. and i-1 as the number of bottles left.
+  //if we have more than 1 bottle left. We print the following message: and i-1 as the number of bottles left on the wall.
   else{
   console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. "Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
   } 
 }
   
 }
-//call/execute the function
+//call-execute the function
 beerSong();
 
