defbddd71662ded24bac1291f9a7b0bc851e433b
Diana Tineo <diana.tineo1@gmail.com>
2022-05-08 15:22:58 -0400

    added function to print lyrics

--- index.js
+++ index.js
@@ -4,7 +4,16 @@
 function beerSong() {
 //keep track of the number of bottles by storing the initial value in a variable called i and working our way down 
 //by subtracting one until there are no more bottles left.
-
+for (let i = 99; i >= 0; i--) {
+  //if we have 0 bottles left. We print the following message:
+  if(i === 0) {
+    console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  }
+  //if we have more than 1 bottle left. We print the following: we use i as the current number of bottles. and i-1 as the number of bottles left.
+  else{
+  console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. "Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
+  } 
+}
   
 }
 beerSong();
